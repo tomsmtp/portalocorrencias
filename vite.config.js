722f4d395f8aca63288503c3aadf3da567812c7d
Ailton Silva Leme <ailton.leme@agt.com.br>
2026-05-29 04:45:23 -0300

    chore: Rebuild com base configurado para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 })
